e1af1e21b5f02f891368e40179dd8487b0a23e63
James Gatannah <james.gatannah@gmail.com>
2014-02-09 14:25:48 -0600

    Get the basic vagrant VM provisioning, more or less. Really needs some pieces to run as a sudoer, others as a regular user. Stop this approach and switch to using ansible

--- config/vagrant/bootstrap.sh
+++ config/vagrant/bootstrap.sh
@@ -18,6 +18,12 @@ apt-get install -y libtool
 apt-get install -y autoconf
 # TODO: Why does autogen.sh ignore this?
 apt-get install -y gettext
+# Strictly speaking, this next package is optional. But getting 0mq
+# man pages is nice.
+# Not nice enough to be worth the massive download. This thing's
+# more ridiculous than emacs.
+#apt-get install -y asciidoc
+apt-get install -y g++
 
 echo "export PATH=$PATH:/home/vagrant/bin" >> /home/vagrant/.bashrc
 
@@ -25,7 +31,7 @@ wget https://raw.github.com/technomancy/leiningen/stable/bin/lein -O /home/vagra
 chmod u+x /home/vagrant/bin/lein
 # This makes sense if this script is running as the vagrant user.
 # Note so much if it's some sort of generic cloud-init sort of situation
-lein
+/home/vagrant/bin/lein
 
 mkdir projects
 cd projects
@@ -37,8 +43,35 @@ cd projects
 
 mkdir base
 cd base
+
+# Here's where trying this as shell scripts really falls apart at the seams:
+# The basic user should be doing everything except the install.
+# Then again, this is all about the dev user anyway. So who cares?
+# (That's a really horrible attitude to take, but I have bigger fish to fry
+# at the moment)
 git clone https://github.com/jimrthy/libsodium.git
-git clone https://github.com/jimrthy/libzmq.git
+cd libsodium
+./autogen.sh
+./configure
+make && make check && make install
+cd ..
+
+git clone https://github.com/jimrthy/zeromq4-x.git
+cd zeromq4-x
+./autogen.sh
+./configure
+# This part seems to be failing miserably and sending me into an infinite loop.
+# Considering all the other moving parts I'm dealing with, I'm starting to
+# strongly suspect that I should be using a different upstream repo here
+# Yeah...
+make 
+# that definitely causes some serious issues. I should not be trying to deal
+# with whatever's happening in this repo. It's making it impossible to deal
+# with basic Curve stuff.
+make check && make install
+cd ..
+
+# Building this actually gets pretty tricky
 git clone https://github.com/jimrthy/jzmq.git
 
 cd ..
