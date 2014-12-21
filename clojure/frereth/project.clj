2b12743cd2b9baba00a4dfc3ce181efbf8f7bdd8
James Gatannah <james.gatannah@gmail.com>
2014-12-20 22:41:38 -0600

    Dependency version bump
    Along with some notes

--- clojure/frereth/project.clj
+++ clojure/frereth/project.clj
@@ -5,20 +5,20 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.cemerick/pomegranate "0.3.0"]
                  [com.stuartsierra/component "0.2.2"]
-                 [com.taoensso/timbre "3.2.1"]
+                 [com.taoensso/timbre "3.3.1"]
                  [frereth-renderer "0.0.1-SNAPSHOT"]
                  [frereth-server "0.1.0-SNAPSHOT"]
                  [im.chit/ribol "0.4.0"]
                  [org.clojure/clojure "1.7.0-alpha1"]
-                 [prismatic/schema "0.2.6"]]
+                 [prismatic/schema "0.3.3"]]
   :main frereth.core
   :profiles {:uberjar {:aot :all}
              :dev {:source-paths ["dev"]
                    :dependencies  [[clj-ns-browser "1.3.1"]
                                    [midje "1.6.3"]
-                                   [org.clojure/tools.namespace "0.2.6"]
+                                   [org.clojure/tools.namespace "0.2.8"]
                                    [org.clojure/java.classpath "0.2.2"]
                                    ;; Umm...do I really not want this for
                                    ;; real??
-                                   [org.clojure/tools.logging "0.2.6"]]}}
+                                   [org.clojure/tools.logging "0.3.1"]]}}
   :repl-options {:init-ns user})
---
+++ config/notes.md
@@ -0,0 +1,13 @@
+Containers:
+
+* baseline - freshly cloned
+* installable - What is this?
+* frereth - baseline that has the basic users added and software installed
+* frereth-just-repositories - I had issues cloning git repos. So this seemed
+  like a worthwhile milestone to flag.
+* frereth-salty - has libsodium and libzmq installed and, at least
+  theoretically, ready to go
+* frereth-built-jzmq : Have jzmq with my curve wrapper installed into the local
+  maven repo
+* frereth-work - what I'm working on. Should spawn off milestones as I go
+
