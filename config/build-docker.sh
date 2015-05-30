ef5045eb19c359fc4b6cba0854632729c5232d50
James Gatannah <james.gatannah@gmail.com>
2015-05-29 20:35:33 -0500

    Create toy docker image
    Actually, it's almost the exact opposite of a toy. It's a very
    broad shotgunned image with my entire development environment.
    
    So, it's an awful start. But at least it's a start.

---
+++ config/build-docker.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Note that this is horribly inefficient. Really need to clean up all the
+# stuff that we won't actually be using
+sudo tar -C $HOME/.local/share/lxc/frereth-work/rootfs -cz . -f frereth-worker.tar.gz
+
+sudo cat frereth-worker.tar.gz | docker import - frereth/development
+
