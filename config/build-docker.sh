e2c844820bb5b314a6be11846e922262e324c3b8
James Gatannah <james.gatannah@gmail.com>
2015-10-24 23:27:02 -0500

    Baby steps toward unprivileged lxc install
    I'm sure this was basically working a year ago, but
    lots of the details have changed since then

--- config/build-docker.sh
+++ config/build-docker.sh
@@ -4,5 +4,6 @@
 # stuff that we won't actually be using
 sudo tar -C $HOME/.local/share/lxc/frereth-work/rootfs -cz . -f frereth-worker.tar.gz
 
+# Q: Does this actually work?
 sudo cat frereth-worker.tar.gz | docker import - frereth/development
 
