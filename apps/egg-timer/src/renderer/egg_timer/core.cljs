62cde33260bec2b2e6284a184bd683cb049e1acd
James Gatannah <james.gatannah@gmail.com>
2016-10-02 17:22:15 +0000

    Play w/ devcards to get a basic idea

--- apps/egg-timer/src/renderer/egg_timer/core.cljs
+++ apps/egg-timer/src/renderer/egg_timer/core.cljs
@@ -1,4 +1,5 @@
-(ns egg-timer.core)
+(ns egg-timer.core
+  (:require-macros [devcards.core :refer (defcard dom-node)]))
 
 (enable-console-print!)
 
@@ -6,10 +7,45 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:text "Hello world!"
+                          :counter 0}))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
 )
+
+(defcard my-first-card
+  "<h1>Devcards rock!</h1>")
+
+(defcard hello
+  "## Documentation
+
+Markdown"
+  {:object "of focus"}
+  {}
+  {})
+
+(defcard state-watcher
+  "Monitor program state"
+  app-state)
+
+(defcard example-dom-node
+  "Should be able to wire up incremental-dom using this approach
+
+Note that changing data-atom in the REPL does not show up in the browser
+until you do something like change the documentation so the updates can register"
+  (dom-node
+   (fn [data-atom node]
+     (set! (.-innerHTML node) (str "<p>Value: " #_(:counter @data-atom) @data-atom " (end)</p>"))))
+  app-state)
+
+(defcard button
+  "Incrementer"
+  (dom-node
+   (fn [data-atom node]
+     (set! (.-innerHTML node)
+           ;; OK, how do I make onClick increment my counter?
+           ;; (this is really why I need a DOM library)
+           (str "<button onClick=\"alert('click')\">+</button>")))))
