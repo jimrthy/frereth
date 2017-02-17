7da84fb41144f43b02d3115a60db677fab1489f3
James Gatannah <james.gatannah@gmail.com>
2017-02-17 04:54:17 +0000

    Add basic unit test for verifying schema install
    Well, at least it verifies that the transaction to do the installation
    completed successfully

--- apps/goal-tender2/src/goal_tender2/core.clj
+++ apps/goal-tender2/src/goal_tender2/core.clj
@@ -10,7 +10,43 @@
     :db/cardinality :db.cardinality/one
     :db/index true
     :db/fulltext true
-    :db/doc "Get the basic idea at a glance"}])
+    :db/doc "Get the basic idea at a glance"}
+   {:db/ident :todo/due-date
+    :db/valueType :db.type/instant
+    :db/cardinality :db.cardinality/one
+    :db/index true
+    :db/doc "When must this task be done?"}
+   {:db/ident :todo/just-a-dream
+    :db/valueType :db.type/boolean
+    :db/cardinality :db.cardinality/one
+    :db/index true
+    :db/doc "Is this something that's just been jotted down, to be considered later?"}
+   {:db/ident :todo/done
+    :db/valueType :db.type/boolean
+    :db/cardinality :db.cardinality/one
+    :db/index true
+    :db/doc "Has user finished this line item yet?"}
+   {:db/ident :todo/tag
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/many
+    :db/index true
+    ;; TODO: Run some performance measurements.
+    ;; Does this have any meaningful impact?
+    ;;:db/fulltext true
+    :db/doc "Help keyword search"}
+   ;; TODO: The entire point is that this needs to be a graph
+   ;; I vaguely remember that a :db.cardinality/one :db.type/ref
+   ;; is actually a 1-* relationship.
+   ;; And that this is setting up a *-*.
+   ;; Honestly, I'm not sure which makes the most sense here,
+   ;; though the *-* could get confusing when you mark a grandparent
+   ;; done...do cousins disappear from beneath the other grandparent?
+   {:db/ident :todo/child
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/many
+    ;; Q: Is this worth indexing?
+    :db/index true
+    :db/doc "Because parent/child is an obvious relationship"}])
 
 (defn connect
   [url]
@@ -21,9 +57,15 @@
   [schema conn]
   @(d/transact conn schema))
 
+;; I have a defmethod sitting around somewhere for this.
+;; TODO: Use that instead
+(defn build-url
+  [database-name]
+  (str "datomic:free://localhost:4334/" database-name))
+
 (defn do-installation
   [database-name]
-  (let [url (str "datomic:free://localhost:4334/" database-name)
+  (let [url (build-url database-name)
         connection (connect url)]
     (install (schema) connection)
     connection))
--- apps/goal-tender2/test/goal_tender2/core_test.clj
+++ apps/goal-tender2/test/goal_tender2/core_test.clj
@@ -1,7 +1,13 @@
 (ns goal-tender2.core-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test :refer (deftest is testing)]
+            [datomic.api :as d]
             [goal-tender2.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Basic schema installation succeeds"
+    (let [db-name "something-random"
+          cxn (do-installation db-name)]
+      (try
+        (is true "Yes, we got here")
+        (finally
+          (d/delete-database (build-url db-name)))))))
