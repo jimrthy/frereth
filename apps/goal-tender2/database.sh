434cd5c8af39bc8764961fd3102aa071efe9c83d
James Gatannah <james.gatannah@gmail.com>
2017-02-17 03:46:00 +0000

    Install a basic attribute
    Don't see where the database files are getting created, yet.

---
+++ apps/goal-tender2/database.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+# This is really cheesy, brittle, and tied explicitly to my current dev
+# environment.
+
+# But it's also fitting for the "just get something useful written" mode I'm in
+# tonight.
+
+~/tools/datomic-free-0.9.5561/bin/transactor ~/tools/datomic-free-0.9.5561/config/samples/free-transactor-template.properties
--- apps/goal-tender2/src/goal_tender2/core.clj
+++ apps/goal-tender2/src/goal_tender2/core.clj
@@ -2,9 +2,31 @@
   (:require [datomic.api :as d]))
 
 (defn schema
-  "This really belongs in something like a .EDN file"
+  "Transactions to put schema into the data store
+  This really belongs in something like a .EDN file"
   []
-  )
+  [{:db/ident :todo/summary
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/index true
+    :db/fulltext true
+    :db/doc "Get the basic idea at a glance"}])
+
+(defn connect
+  [url]
+  (d/create-database url)
+  (d/connect url))
+
+(defn install
+  [schema conn]
+  @(d/transact conn schema))
+
+(defn do-installation
+  [database-name]
+  (let [url (str "datomic:free://localhost:4334/" database-name)
+        connection (connect url)]
+    (install (schema) connection)
+    connection))
 
 (defn -main
   "I don't do a whole lot ... yet."
