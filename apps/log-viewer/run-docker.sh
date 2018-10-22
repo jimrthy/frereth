cc99553fdca18caa017774d16ff2712dc753fb11
James Gatannah <james.gatannah@gmail.com>
2018-10-21 22:53:45 -0500

    Set up mount for file watcher inside Container
    So changes to .cljs files get reloaded automatically.

--- apps/log-viewer/run-docker.sh
+++ apps/log-viewer/run-docker.sh
@@ -1,8 +1,16 @@
 #! /bin/sh
 
+echo Trying to mount "`pwd`/src"
 # 9001:  websocket REPL
 # 9009:  nREPL
 # 10555: web server
 # 43043: nrepl port
 # 43140: Boot reload port
-docker run -d  -p 9001:9001 -p 9009:9009 -p 10555:10555 -p 43043:43043 -p 43140:43140 frereth/log-viewer:latest
+docker run -d \
+       -p 9001:9001 \
+       -p 9009:9009 \
+       -p 10555:10555 \
+       -p 43043:43043 \
+       -p 43140:43140 \
+       --mount type=bind,source="`pwd`/src",target=/opt/frereth/log-viewer/src \
+       frereth/log-viewer:latest
