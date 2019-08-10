4922f6ae4ba193913593d857c1d500abf61eae4d
James Gatannah <james.gatannah@gmail.com>
2019-08-10 17:44:37 -0500

    Clone new fulcro template
    For the Sonic Forces tracker

---
+++ apps/racing/sonic-forces/tracker/src/main/tracker/client.cljs
@@ -0,0 +1,30 @@
+(ns tracker.client
+  (:require [fulcro.client :as fc]
+            [tracker.ui.root :as root]
+            [fulcro.client.network :as net]
+            [fulcro.client.data-fetch :as df]))
+
+(defonce SPA (atom nil))
+
+(defn mount []
+  (reset! SPA (fc/mount @SPA root/Root "app")))
+
+(defn start []
+  (mount))
+
+(def secured-request-middleware
+  ;; The CSRF token is embedded via server_components/html.clj
+  (->
+    (net/wrap-csrf-token (or js/fulcro_network_csrf_token "TOKEN-NOT-IN-HTML!"))
+    (net/wrap-fulcro-request)))
+
+(defn ^:export init []
+  (reset! SPA (fc/make-fulcro-client
+                {:client-did-mount (fn [tracker]
+                                     (df/load tracker :all-users root/User))
+                 ;; This ensures your client can talk to a CSRF-protected server.
+                 ;; See middleware.clj to see how the token is embedded into the HTML
+                 :networking       {:remote (net/fulcro-http-remote
+                                              {:url                "/api"
+                                               :request-middleware secured-request-middleware})}}))
+  (start))
